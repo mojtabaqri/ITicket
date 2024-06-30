369123521b01fea6efea145d951afd2d181c04c4
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2024-06-30 19:33:19 +0330

    remove prefix from routes

--- routes/api.php
+++ routes/api.php
@@ -20,55 +20,55 @@ Route::prefix('otp')->group(function () {
 //-----******************************************- Start Admin Routes***********************************
 
 //user management routes
-Route::name('admin.user')->middleware(['auth:api'])->prefix('user')->group(function () {
+Route::name('admin.user')->middleware(['auth:api'])->group(function () {
     Route::resource('user',UserController::class);
 
 });
 
 //Service management routes
-Route::name('admin.services')->middleware(['auth:api'])->prefix('service')->group(function () {
+Route::name('admin.services')->middleware(['auth:api'])->group(function () {
     Route::resource('service',ServiceController::class);
 });
 
 //Service  Category  management routes
-Route::name('admin.services.category')->middleware(['auth:api'])->prefix('service-cat')->group(function () {
+Route::name('admin.services.category')->middleware(['auth:api'])->group(function () {
     Route::resource('service-cat',ServiceCategoryController::class);
 });
 
 //State Location Management   routes
-Route::name('admin.statelocation')->middleware(['auth:api'])->prefix('location')->group(function () {
+Route::name('admin.statelocation')->middleware(['auth:api'])->group(function () {
     Route::resource('location',StateLocationController::class);
 });
 
 
 //Bank Transaction Routes ( Reports , manual Transaction )//wallet transaction routs and bank transaction routes
-Route::name('admin.Transaction')->middleware(['auth:api'])->prefix('transaction')->group(function () {
+Route::name('admin.Transaction')->middleware(['auth:api'])->group(function () {
 });
 
 //booking (manual Booking / report  /
-Route::name('admin.booking')->middleware(['auth:api'])->prefix('booking')->group(function () {
+Route::name('admin.booking')->middleware(['auth:api'])->group(function () {
 
 });
 
 //invoice ( report invoice )
-Route::name('admin.invoice')->middleware(['auth:api'])->prefix('invoice')->group(function () {
+Route::name('admin.invoice')->middleware(['auth:api'])->group(function () {
 
 });
 
 //Notifection Routes
-Route::name('admin.notifection')->middleware(['auth:api'])->prefix('notifection')->group(function () {
+Route::name('admin.notifection')->middleware(['auth:api'])->group(function () {
 
 });
 
 //Review Management  (acept review / review  report )
-Route::name('admin.review')->middleware(['auth:api'])->prefix('review')->group(function () {
+Route::name('admin.review')->middleware(['auth:api'])->group(function () {
 
 });
 //time slot managements
-Route::name('admin.timeslot')->middleware(['auth:api'])->prefix('timeslot')->group(function () {
+Route::name('admin.timeslot')->middleware(['auth:api'])->group(function () {
 
 });
- 
+
 
 //-----******************************************- End Admin Routes***********************************
 
